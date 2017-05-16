0c225a0c24b696798353e24d6c2431129632604f
Christian Halsell <christianhalsell@Christians-MacBook-Pro.local>
2017-05-16 09:55:11 -0700

    set-up conditionals

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+(function () {
+    function checkTime(i) {
+        return (i < 10) ? "0" + i : i;
+    }
+
+    function startTime() {
+        var today = new Date(),
+            h = today.getHours(),
+            m = checkTime(today.getMinutes()),
+            s = checkTime(today.getSeconds()),
+            time = document.getElementById('time'),
+            wrapper = document.getElementById('wrapper');
+
+        time.innerHTML = h + ":" + m;
+        // time.innerHTML = h + ":" + m + ":" + s;
+
+        if (h === 7 && m >= 15 && m < 25) {
+            wrapper.className = 'orange'
+            time.className = 'text-white';
+        } else if (h === 7 && m >= 25) {
+            wrapper.className = 'red';
+        } else {
+            wrapper.className = 'green';
+            time.className = 'text-white';
+        }
+
+        setTimeout(function () {
+            startTime();
+        }, 1000);
+    }
+
+    startTime();
+})();
