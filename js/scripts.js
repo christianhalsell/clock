675531b3d1ec195774378d9f8de1eb18eb43518d
christianhalsell <christianhalsell@gmail.com>
2017-05-29 11:18:01 -0700

    Add yellow time period

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,11 @@
         time.innerHTML = h + ":" + m;
         // time.innerHTML = h + ":" + m + ":" + s;
 
-        if (h === 7 && m >= 15 && m < 25) {
-            wrapper.className = 'orange'
+        if (h === 7 && m >= 5 && m < 15) {
+            wrapper.className = 'yellow';
+            time.className = '';            
+        } else if (h === 7 && m >= 15 && m < 25) {
+            wrapper.className = 'orange';
             time.className = 'text-white';
         } else if (h === 7 && m >= 25) {
             wrapper.className = 'red';
