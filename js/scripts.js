d98b1906ab75a2575ff35538b859e562d325222d
Christian Halsell <christian.halsell@verys.com>
2019-08-28 08:53:07 -0700

    fix red time bug

--- js/scripts.js
+++ js/scripts.js
@@ -15,16 +15,20 @@
             h = h - 12;
         }
 
+        // TEST
+        // h = 7;
+        // m = checkTime(15);
+
         time.innerHTML = h + ":" + m;
         // time.innerHTML = h + ":" + m + ":" + s;
 
-        if (h === 7 && m >= 5 && m < 10) {
+        if (h === 7 && m >= 0 && m < 10) {
             wrapper.className = 'yellow';
             time.className = '';            
         } else if (h === 7 && m >= 10 && m < 15) {
             wrapper.className = 'orange';
             time.className = 'text-white';
-        } else if (h === 7 && m >= 25) {
+        } else if (h === 7 && m >= 15 && m < 59) {
             wrapper.className = 'red';
             time.className = '';
         } else {
