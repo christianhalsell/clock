c07adbec400a322ecd08d618b1f56df32ed4b322
Christian Halsell <shinobiwulf@gmail.com>
2020-01-28 06:36:59 -0800

    swap blue and green

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,7 @@
         time.innerHTML = h + ":" + m;
 
         if (h === 6 && m >= 30 && m < 45) {
-            wrapper.className = 'blue';
+            wrapper.className = 'green';
             time.className = 'text-white';            
         } else if (h === 6 && m >= 45 && m <= 59) {
             wrapper.className = 'yellow';
@@ -33,7 +33,7 @@
             wrapper.className = 'red';
             time.className = 'text-white';
         } else {
-            wrapper.className = 'green';
+            wrapper.className = 'blue';
             time.className = 'text-white';
         }
 
